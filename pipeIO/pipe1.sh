51016f886c7bddd3d46978622ad60af7d46d0fbf
Wen Lin <wenching.lin@zementis.com>
2014-06-26 08:03:54 -0700

    added tr uniq sort example

---
+++ pipeIO/pipe1.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo 'Transform letter and Pipe'
+
+# tr : replacing or removing specific characters , NOT string !!!
+#  -s  change the sequences of identical adjacent characters in its output to a single token
+#  e.g.  tr -s 'n' replaces sequences of one or more newline characters with a single newline.
+#  -c  complement of complements the first set of characters
+#  -cs change all non-alphabet to 
+#
+# sort: put the lines in alphabetic order
+
+tr 'A-Z' 'a-z' < input.txt | tr -cs 'a-z' '\n' | sort  
